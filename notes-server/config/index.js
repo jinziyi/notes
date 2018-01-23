6329565c7eaa610d7b6b7051df12fac20cbd6747
jinziyi <996757810@qq.com>
2018-01-23 16:17:31 +0800

    server work

--- notes-server/config/index.js
+++ notes-server/config/index.js
@@ -7,8 +7,9 @@ const production_env = require('./production');
 const test_env = require('./test');
 
 //根据不同的NODE_ENV，输出不同的配置对象，默认输出development的配置对象
+console.log(process.env.NODE_ENV);
 module.exports = {
     development: development_env,
 	production: production_env,
     test: test_env
-}[process.env.NODE_ENV || 'development'];
+}[process.env.NODE_ENV || 'productoin'];
