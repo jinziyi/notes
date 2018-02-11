73156b6a5eac9b6de1f28ad7fa0ae1b6001552ab
陈静宇 <996757810@qq.com>
2018-02-11 11:06:32 +0800

    rebuild the dist

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ ReactDOM.render(<AppContainer warnings={false}><App/></AppContainer>, document.g
 
 	if ('serviceWorker' in navigator) {
 		window.addEventListener('load', function () {
-			navigator.serviceWorker.register('/PWA/sw.js').then(function (registration) {
+			navigator.serviceWorker.register('../PWA/sw.js').then(function (registration) {
 				console.log('ServiceWorker registration successful with scope: ', registration.scope);
 			}).catch(function (err) {
 				console.log('ServiceWorker registration failed: ', err);
