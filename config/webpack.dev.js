03f529a2b05aa2cb291cd758537a844e44b5bd69
user <user@userdeMacBook-Pro-5.local>
2018-03-28 15:20:49 +0800

    config update

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,56 +7,57 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-	// devtool: 'eval-source-map',
-	devtool: 'inline-source-map',
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, '../dist'),
-		publicPath: '/',
-	},
-	devServer: {
-		stats: {
-			assets: true,
-			modules: false,
-		},
-		contentBase: path.resolve(__dirname, '../dist'),
-		historyApiFallback: true,
-		hot: true,
-		host: '0.0.0.0',
-		proxy: {
-			"/api": "http://localhost:3001"
-		},
-		port: 3000,
-		clientLogLevel: 'info',
-		compress: true,
-	},
-	plugins: [
-		new webpack.NamedModulesPlugin(),
-		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify('development')
-			}
-		}),
-		new webpack.HotModuleReplacementPlugin(),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.s?css$/,
-				use: ['style-loader',
-					{
-						loader: 'css-loader',
-					}, 'sass-loader', {
-						loader: 'postcss-loader',
-						options: {
-							ident: 'postcss',
-							plugins: (loader) => [
-								require('postcss-cssnext')(),
-							]
-						}
-					},
-				]
-			},
-		]
-	}
+  // devtool: 'eval-source-map',
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
+  },
+  devServer: {
+    stats: {
+      assets: true,
+      modules: false,
+    },
+    contentBase: path.resolve(__dirname, '../dist'),
+    historyApiFallback: true,
+    hot: true,
+    host: '0.0.0.0',
+    proxy: {
+      '/api': 'http://localhost:3001'
+    },
+    useLocalIp: true,
+    port: 3000,
+    clientLogLevel: 'info',
+    compress: true,
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: ['style-loader',
+          {
+            loader: 'css-loader',
+          }, 'sass-loader', {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: (loader) => [
+                require('postcss-cssnext')(),
+              ]
+            }
+          },
+        ]
+      },
+    ]
+  }
 });
